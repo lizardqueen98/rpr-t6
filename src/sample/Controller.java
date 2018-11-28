e02a94c5df50404e7c569f7989e04963eb80e44f
lizardqueen98 <nadija.b@gmail.com>
2018-11-29 00:11:04 +0100

    <3

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,6 +2,8 @@ package sample;
 
 import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 
 import java.awt.event.ActionEvent;
@@ -13,16 +15,33 @@ public class Controller {
 
     public TextField prezime;
     public TextField broj_indeksa;
-    private SimpleStringProperty name, last_name, index;
+    public TextField jmbg;
+    public TextField datum;
+    public ComboBox izbor;
+    public TextField adresa;
+    public TextField telefon;
+    public TextField mail;
+    private SimpleStringProperty name, last_name, index, JMBG, date, adress, phone, email;
     public Controller(){
         name=new SimpleStringProperty();
         last_name=new SimpleStringProperty();
         index=new SimpleStringProperty();
+        JMBG=new SimpleStringProperty();
+        date=new SimpleStringProperty();
+        adress=new SimpleStringProperty();
+        phone=new SimpleStringProperty();
+        email=new SimpleStringProperty();
     }
     public void initialize() {
         ime.textProperty().bindBidirectional(name);
         prezime.textProperty().bindBidirectional(last_name);
         broj_indeksa.textProperty().bindBidirectional(index);
+        jmbg.textProperty().bindBidirectional(JMBG);
+        datum.textProperty().bindBidirectional(date);
+        izbor.getItems().addAll("Sarajevo", "Mostar", "Zenica", "Foca", "Tuzla", "Doboj");
+        adresa.textProperty().bindBidirectional(adress);
+        telefon.textProperty().bindBidirectional(phone);
+        mail.textProperty().bindBidirectional(email);
     }
     public SimpleStringProperty nameProperty() {
         return name;
@@ -42,13 +61,26 @@ public class Controller {
     public String getIndex() {
         return index.get();
     }
-    public void unesi(){
-        nameProperty().setValue(ime.getText());
-        lastNameProperty().setValue(prezime.getText());
-        indexProperty().setValue(broj_indeksa.getText());
+    public SimpleStringProperty JMBGProperty() {
+        return JMBG;
+    }
+    public String getJMBG() {
+        return JMBG.get();
+    }
+    public SimpleStringProperty dateProperty() {
+        return date;
+    }
+    public String getDate() {
+        return date.get();
+    }
+    public SimpleStringProperty adressProperty(){
+        return adress;
+    }
+    public String getAdress(){
+        return adress.get();
     }
     public void potvrdi(javafx.event.ActionEvent actionEvent) {
-        System.out.println(getName()+" "+getLastName()+" "+getIndex());
+        System.out.println(getName()+" "+getLastName()+" "+getIndex()+" "+getJMBG()+" "+getDate()+" "+izbor.getValue());
     }
 
 }
